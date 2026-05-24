19575ae29779d5f8ea5eac1f0d5569dcb6bab0b9
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-24 10:58:50 +0530

    variables added

---
+++ file2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This is another simple shell script
+
+person1=$1
+person2=$2
+
+echo "$person1: Hey $person2, How are you doing?"
+echo "$person2: I'm doing well, $person1. How about you?"
+echo "$person1: I'm doing great, $person2. Today is sunday so what's the plan?
+echo "$person2: I was thinking we could go to the park and have a picnic."
+echo "$person1: That sounds like a great idea, $person2. Let's do it!"

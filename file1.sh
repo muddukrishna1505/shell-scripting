16123f001bb9d2f32f1cc4535e70264892d3cd5e
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-24 08:07:06 +0530

    variables added

--- file1.sh
+++ file1.sh
@@ -3,9 +3,14 @@ echo "Hello, world!"
 echo "This is a sample shell script."
 
 # Adding a simple conversation between two people
+#Using variables to store the conversation
+person1="Trump"
+person2="Iran"
 
-Trump: Hey Iran, I'll blast you.
-Iran: Oh really? You think you can do that?
-Trump: Absolutely! I've got the best missiles, the best military. You don't stand a chance.
-Iran: Don't underestimate us, Trump. We have our own capabilities and allies.
-Trump: Well, we'll see about that. I'm not afraid of you.
+Trump: Hey $person2, I'll blast you.
+Iran: Oh really? You think you can do that $person1?
+Trump: Absolutely $person2. I've got the best missiles, the best military. You don't stand a chance.
+Iran: Don't underestimate us, $person1. We have our own capabilities and allies.
+Trump: Well $person2, we'll see about that. I'm not afraid of you.
+Iran: You should be. We have a strong defense system and we're not afraid to use it $person1.
+Trump: We'll see about that. I'm not backing down $person2.

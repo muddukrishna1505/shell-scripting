b4a18e898e86fb91922f93df90acc503d2a5aedd
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-24 12:53:59 +0530

    variables added

--- file4.sh
+++ file4.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-time=$(date ) #time should be human readable, not in seconds
-echo "Current time: $time"
+#time=$(date ) #time should be human readable, not in seconds
+#echo "Current time: $time"
+
+#I want to see task execution time
+start_time=$(date +%s) #time in seconds
+# Simulating a task by sleeping for 5 seconds
+sleep 10
+end_time=$(date +%s) #time in seconds   
+execution_time=$((end_time - start_time)) #calculating execution time in seconds
+echo "Task execution time: $execution_time seconds"

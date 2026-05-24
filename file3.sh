afaa9f6b1c66d9c0b0865f191a759a0ca6b70253
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-24 12:16:14 +0530

    variables added

---
+++ file3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Using read to get user input for the conversation
+
+echo "Enter the User name:"
+read USER_NAME
+echo "Enter the password:"
+read -s PASSWORD
+
+echo "Print the username is $USER_NAME"
+echo "print the password is $PASSWORD"

8704073c3ac6081c3626217085687936fb3a5017
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-24 07:52:41 +0530

    conversation added

--- file1.sh
+++ file1.sh
@@ -1,3 +1,11 @@
 # This is a simple shell script
 echo "Hello, world!"
 echo "This is a sample shell script."
+
+# Adding a simple conversation between two people
+
+Trump: Hey Iran, I'll blast you.
+Iran: Oh really? You think you can do that?
+Trump: Absolutely! I've got the best missiles, the best military. You don't stand a chance.
+Iran: Don't underestimate us, Trump. We have our own capabilities and allies.
+Trump: Well, we'll see about that. I'm not afraid of you.

dbef146abe52f20d0f8662a21e619f19a3cbea95
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-24 08:45:06 +0530

    variables added

--- file1.sh
+++ file1.sh
@@ -8,10 +8,10 @@ echo "This is a sample shell script."
 person1="Trump"
 person2="Iran"
 
-Trump: Hey $person2, I'll blast you.
-Iran: Oh really? You think you can do that $person1?
-Trump: Absolutely $person2. I've got the best missiles, the best military. You don't stand a chance.
-Iran: Don't underestimate us, $person1. We have our own capabilities and allies.
-Trump: Well $person2, we'll see about that. I'm not afraid of you.
-Iran: You should be. We have a strong defense system and we're not afraid to use it $person1.
-Trump: We'll see about that. I'm not backing down $person2.
+echo "Trump: Hey $person2, I'll blast you."
+echo "Iran: Oh really? You think you can do that $person1?"
+echo "Trump: Absolutely $person2. I've got the best missiles, the best military. You don't stand a chance."
+echo "Iran: Don't underestimate us, $person1. We have our own capabilities and allies."
+echo "Trump: Well $person2, we'll see about that. I'm not afraid of you."
+echo "Iran: You should be. We have a strong defense system and we're not afraid to use it $person1."
+echo "Trump: We'll see about that. I'm not backing down $person2."

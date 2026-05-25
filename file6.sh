ebfcc839c648bc6c5855abfa9401982f147c02a4
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-25 22:17:16 +0530

    checking with special variabl

--- file6.sh
+++ file6.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-
+#Printing special variables'
 echo "Hello $1 and $2"
+echo "The name of the script is $0"
+echo "The number of arguments passed is $#"
+echo "All variables passed through  $@"
+echo "The process id of the script is $$"
+echo "The exit status of the last command is $?"
+echo "The first argument is $1"
+echo "The second argument is $2"    
+echo "Who is running thid script? $USER"
+echo "The home directory of the user is $HOME"

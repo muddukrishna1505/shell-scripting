1c95b73d05020e098147c9456079053e9d5c1708
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-26 22:14:41 +0530

    array concept

--- file8.sh
+++ file8.sh
@@ -14,19 +14,18 @@
 echo "What is the day today?: "
 read day
 
-
 # Convert input to lowercase
 day=$(echo "$day" | tr 'A-Z' 'a-z')
 
-if [ "$day" == "Sunday" ]; then
-    echo -e "Today is $day, \e[33mTake holiday and enjoy\e[0m"
+if [ "$day" == "sunday" ]; then
+    echo -e "Today is Sunday, \e[31mTake holiday and enjoy\e[0m"
 
-elif [ "$day" == "Monday" ] || [ "$day" == "Tuesday" ] || \
-     [ "$day" == "Wednesday" ] || [ "$day" == "Thursday" ] || \
-     [ "$day" == "Friday" ] || [ "$day" == "Saturday" ]; then
+elif [ "$day" == "monday" ] || [ "$day" == "tuesday" ] || \
+     [ "$day" == "wednesday" ] || [ "$day" == "thursday" ] || \
+     [ "$day" == "friday" ] || [ "$day" == "saturday" ]; then
 
     echo -e "Today is $day, \e[32mGo to school and study well\e[0m"
 
 else
-    echo -e "\e[31mInvalid input! Please enter a valid day.\e[0m"
+    echo -e "\e[33mInvalid input! Please enter a valid day.\e[0m"
 fi

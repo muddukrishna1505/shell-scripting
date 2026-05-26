b696a57fafe4c826372cdb7f8a489ad911837cfe
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-26 22:03:34 +0530

    array concept

--- file8.sh
+++ file8.sh
@@ -15,7 +15,7 @@ echo "What is the day today?: "
 read day
 
 if [ "$day" == "Sunday" ]; then
-    echo -e "Today is $day, \e[31mTake holiday and enjoy\e[0m"
+    echo -e "Today is $day, \e[33mTake holiday and enjoy\e[0m"
 
 elif [ "$day" == "Monday" ] || [ "$day" == "Tuesday" ] || \
      [ "$day" == "Wednesday" ] || [ "$day" == "Thursday" ] || \
@@ -24,5 +24,5 @@ elif [ "$day" == "Monday" ] || [ "$day" == "Tuesday" ] || \
     echo -e "Today is $day, \e[32mGo to school and study well\e[0m"
 
 else
-    echo -e "\e[33mInvalid input! Please enter a valid day.\e[0m"
+    echo -e "\e[31mInvalid input! Please enter a valid day.\e[0m"
 fi

f581d0cc3b9377aef2511679b7a535fb9cedd8c7
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-26 20:16:37 +0530

    array concept

--- file7.sh
+++ file7.sh
@@ -1,7 +1,12 @@
 #!/bin/bash #shebang line
 
-Num1=10
-Num2=20
+# Num1=10
+# Num2=20
 
-Sum=$((Num1 + Num2))
-echo "The sum of Num1 and Num2 is: $Sum"
+# Sum=$((Num1 + Num2))
+# echo "The sum of Num1 and Num2 is: $Sum"
+
+#Array
+
+Movies=("Bahubali" "KGF" "RRR" "Vikram" "Ponniyin Selvan")
+echo "The first movie is: ${Movies[0]}"

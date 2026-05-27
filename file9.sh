4452d9e8fba72a68a7d4421b1d4e0f1a16283644
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-27 19:48:05 +0530

    checking the user id

--- file9.sh
+++ file9.sh
@@ -5,21 +5,24 @@ if [ $USERID -ne 0 ]; then
     echo -e "\e[31mYou are not a root user. Please run this script as a root user\e[0m"
     exit 1
 fi
-# echo -e "\e[32mYou are a root user. You can run this script\e[0m"
-# echo -e "\e[32mI'm continuing ...\e[0m"
+
+#arg1 - what are you trying to install
+#arg2 - exit code
+
+validate() {
+    if [ $2 -ne 0 ]; then
+        echo -e "\e[31m$1 server installation failed\e[0m"
+        exit 1
+
+        else
+        echo -e "$1 server installation \e[32msuccessful\e[0m"
+    fi
+}
 mysql --version
 
 if [ $? -eq 0 ]; then
     echo -e "\e[35mMYSQL is already installed ... Skipping installation\e[0m"
 
 else
-    echo "Installing MYSQL server ..."
-    dnf install mysql-server -y 
-
-    if [ $? -ne 0 ]; then
-        echo -e "\e[31mMYSQL server installation failed\e[0m"
-        exit 1
-    fi
-     
-    echo -e "MYSQL server installation \e[32msuccessful\e[0m"
+validate "MYSQL" $?
 fi

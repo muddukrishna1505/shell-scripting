5048a212d27f41d5fc8650619dbca32b91d44ecd
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-27 06:30:42 +0530

    checking the user id

--- file9.sh
+++ file9.sh
@@ -5,5 +5,5 @@ if [ $USERID -ne 0 ]; then
     echo "You are not a root user. Please run this script as a root user"
     exit 1
 fi
-
+echo "You are a root user. You can run this script"
 echo "I'm continuing ..."

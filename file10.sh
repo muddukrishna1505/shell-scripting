c69732fe469b10092fb54925ebd60ff3eb8a1091
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-05-28 05:50:19 +0530

    checking the user id

--- file10.sh
+++ file10.sh
@@ -3,24 +3,19 @@
 LOGS_DIR=/root/logs
 LOGS_FILE="$LOGS_DIR/$0.log"
 
+validate() {
+    if [ $? -eq 0 ]; then
+    echo -e "\e[35m$1 is already installed\e[0m"
+    else
+    echo -e "\e[31m$1 is not installed\e[0m"
+fi
+}
 mysql --version >> $LOGS_FILE
 
-if [ $? -eq 0 ]; then
-    echo -e "\e[35mMYSQL is already installed\e[0m"
-    else
-    echo -e "\e[31mMYSQL is not installed\e[0m"
-fi
+validate "MYSQL"
 
 nginx --version >> $LOGS_FILE
-if [ $? -eq 0 ]; then
-    echo -e "\e[35mNGINX is already installed\e[0m"
-    else
-    echo -e "\e[31mNGINX is not installed\e[0m"
-fi
+validate "NGINX"
 
 apache2 -v >> $LOGS_FILE
-if [ $? -eq 0 ]; then
-    echo -e "\e[35mAPACHE is already installed\e[0m"
-    else
-    echo -e "\e[31mAPACHE is not installed\e[0m"
-fi
+validate "APACHE2"
